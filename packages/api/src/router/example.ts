841f957e425385e91137cf2b3eedc4b3fbab8aa9
Caleb Rivera <calebrossr@gmail.com>
2023-11-24 05:09:35 -0500

    await query

--- packages/api/src/router/example.ts
+++ packages/api/src/router/example.ts
@@ -1,7 +1,7 @@
 import { t } from "../trpc";
 
 export const exampleRouter = t.router({
-  ping: t.procedure.query(({ ctx }) => {
-    return ctx.db.query.users.findMany();
+  ping: t.procedure.query(async ({ ctx }) => {
+    return await ctx.db.query.users.findMany();
   }),
 });
