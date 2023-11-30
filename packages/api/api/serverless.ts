d91e6b6dfcfe5d2419effef48e8e310eefb69eb6
Caleb Rivera <calebrossr@gmail.com>
2023-11-30 07:17:39 -0500

    remove prefix

--- packages/api/api/serverless.ts
+++ packages/api/api/serverless.ts
@@ -9,9 +9,7 @@ const app = Fastify({
 });
 
 // Register your application as a normal plugin.
-app.register(import("../src/app"), {
-  prefix: "/",
-});
+app.register(import("../src/app"));
 
 export default async (req: Request, res: Response) => {
   await app.ready();
