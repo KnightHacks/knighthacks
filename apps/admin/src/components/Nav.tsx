b775dbada79aaff2a67f183db4b4e283e3fa6dad
Caleb Rivera <calebrossr@gmail.com>
2023-12-09 21:47:56 -0500

    add simple navbar

--- apps/admin/src/components/Nav.tsx
+++ apps/admin/src/components/Nav.tsx
@@ -1,3 +1,22 @@
+import { Link } from "wouter";
+
 export function Nav() {
-  return <>Hello, World!</>;
+  return (
+    <div className="m-4">
+      <ul>
+        <li>
+          <Link href="/">Overview</Link>
+        </li>
+        <li>
+          <Link href="/users">Users</Link>
+        </li>
+        <li>
+          <Link href="/hackers">Hackers</Link>
+        </li>
+        <li>
+          <Link href="/hackathons">Hackathons</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
