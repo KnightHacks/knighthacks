0430327c98861fd2e0410e251eb53f038ff5e501
Caleb Rivera <calebrossr@gmail.com>
2023-12-10 23:44:00 -0500

    add sign up page

--- apps/admin/src/components/Nav.tsx
+++ apps/admin/src/components/Nav.tsx
@@ -19,6 +19,9 @@ export function Nav() {
         <li>
           <Link href="/signin">Sign In</Link>
         </li>
+        <li>
+          <Link href="/signup">Sign Up</Link>
+        </li>
       </ul>
     </div>
   );
---
+++ apps/admin/src/pages/SignUp.tsx
@@ -0,0 +1,5 @@
+import { SignUp as ClerkSignUp } from "@clerk/clerk-react";
+
+export function SignUp() {
+  return <ClerkSignUp />;
+}
