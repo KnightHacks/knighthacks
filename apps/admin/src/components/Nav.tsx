da0fd266e7b2144dea8ed462ee5993f244576209
Caleb Rivera <calebrossr@gmail.com>
2023-12-12 05:00:28 -0500

    renove auth0 from nav component

--- apps/admin/src/components/Nav.tsx
+++ apps/admin/src/components/Nav.tsx
@@ -1,9 +1,5 @@
 import { Link } from "wouter";
-import { useAuth0 } from "@auth0/auth0-react";
-
 export function Nav() {
-  const { isAuthenticated, logout } = useAuth0();
-
   return (
     <div className="m-4">
       <ul>
@@ -19,25 +15,6 @@ export function Nav() {
         <li>
           <Link href="/hackathons">Hackathons</Link>
         </li>
-        {isAuthenticated ? (
-          <li>
-            <button
-              onClick={() =>
-                logout({
-                  logoutParams: {
-                    returnTo: window.location.origin,
-                  },
-                })
-              }
-            >
-              Log out
-            </button>
-          </li>
-        ) : (
-          <li>
-            <Link href="/signin">Sign In</Link>
-          </li>
-        )}
       </ul>
     </div>
   );
