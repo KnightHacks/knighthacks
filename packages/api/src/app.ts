298f90b8db893cb7fdfff26d7fa03be3753895e4
Caleb Rivera <calebrossr@gmail.com>
2023-12-12 05:12:17 -0500

    remove clerk auth

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./routers";
 import { cors } from "hono/cors";
@@ -8,24 +7,8 @@ import { createContext } from "./context";
 const app = new Hono();
 
 app.use("*", cors());
-app.use("*", clerkMiddleware());
 app.get("/", (c) => {
   return c.text("Hello World!");
-});
-
-app.get("/", (c) => {
-  const auth = getAuth(c);
-
-  if (!auth?.userId) {
-    return c.json({
-      message: "You are not logged in.",
-    });
-  }
-
-  return c.json({
-    message: "You are logged in!",
-    userId: auth.userId,
-  });
 });
 
 app.use(
