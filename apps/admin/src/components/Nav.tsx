66c509a06cfb848471004ca2e34d7a161b1dad0f
Caleb Rivera <calebrossr@gmail.com>
2023-12-11 23:03:17 -0500

    render out different button based on auth state

--- apps/admin/src/components/Nav.tsx
+++ apps/admin/src/components/Nav.tsx
@@ -1,6 +1,9 @@
 import { Link } from "wouter";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Nav() {
+  const { isAuthenticated, logout } = useAuth0();
+
   return (
     <div className="m-4">
       <ul>
@@ -16,9 +19,25 @@ export function Nav() {
         <li>
           <Link href="/hackathons">Hackathons</Link>
         </li>
-        <li>
-          <Link href="/signin">Sign In</Link>
-        </li>
+        {isAuthenticated ? (
+          <li>
+            <button
+              onClick={() =>
+                logout({
+                  logoutParams: {
+                    returnTo: window.location.origin,
+                  },
+                })
+              }
+            >
+              Log out
+            </button>
+          </li>
+        ) : (
+          <li>
+            <Link href="/signin">Sign In</Link>
+          </li>
+        )}
       </ul>
     </div>
   );
