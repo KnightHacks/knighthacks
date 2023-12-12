1f1887879a5191f601121a550738f8be9a776330
Caleb Rivera <calebrossr@gmail.com>
2023-12-12 03:24:14 -0500

    configure cors

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,8 +1,11 @@
 import { Hono } from "hono";
 import { trpcServer } from "@hono/trpc-server";
 import { appRouter } from "./routers";
+import { cors } from "hono/cors";
 
 const app = new Hono();
+
+app.use("*", cors());
 
 app.get("/", (c) => {
   return c.text("Hello World!");
