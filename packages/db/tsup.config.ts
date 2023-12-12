51535880bb36807591ef50f9b79fb70538745db0
Caleb Rivera <calebrossr@gmail.com>
2023-12-11 23:02:46 -0500

    bundle db in dev

---
+++ packages/db/tsup.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: ["index.ts"],
+  format: ["cjs", "esm"],
+  outDir: "dist",
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+});
