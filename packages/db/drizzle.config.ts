c29bc81b61eda53317b32a2b2b5aa2bdea5397b6
Caleb Rivera <calebrossr@gmail.com>
2023-12-12 05:17:40 -0500

    remove unnecessary logs

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,6 @@
 import { defineConfig } from "drizzle-kit";
 import "dotenv/config";
 
-console.log("Hello from drizzle.config.ts");
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
   out: "./drizzle",
   schema: ["./schemas/schema.ts"],
