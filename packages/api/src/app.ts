7e1a470cd605d25d6f31c9772d1a31b68edb711b
Caleb Rivera <calebrossr@gmail.com>
2023-12-11 23:46:05 -0500

    handle root on api

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -8,6 +8,12 @@ const app = express();
 
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello from Express!",
+  });
+});
+
 app.use(
   "/trpc",
   trpcExpress.createExpressMiddleware({
