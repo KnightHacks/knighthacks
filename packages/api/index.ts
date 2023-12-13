66341a6bb868d43994c0df413e1ba663dafcd88d
Caleb Rivera <calebrossr@gmail.com>
2023-12-13 06:05:29 -0500

    export router types

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -1,2 +1,8 @@
-export { type AppRouter } from "./src/routers";
+import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
+import { AppRouter } from "./src/routers";
+
+export { type AppRouter };
 export { type Context } from "./src/context";
+
+export type RouterInput = inferRouterInputs<AppRouter>;
+export type RouterOutput = inferRouterOutputs<AppRouter>;
