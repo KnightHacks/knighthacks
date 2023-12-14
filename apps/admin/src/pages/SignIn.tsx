9b0e71918db6af6747d45f875ff2bf1a6fa8cd13
Caleb Rivera <calebrossr@gmail.com>
2023-12-13 22:53:15 -0500

    create sign in with google option

--- apps/admin/src/pages/SignIn.tsx
+++ apps/admin/src/pages/SignIn.tsx
@@ -1,3 +1,32 @@
+import { supabase } from "@/lib/supabase";
+import { useSession } from "@/lib/hooks/useSession";
+
 export function SignIn() {
-  return <></>;
+  const { session } = useSession();
+
+  if (!session) {
+    return (
+      <button
+        onClick={() => {
+          supabase.auth.signInWithOAuth({
+            provider: "google",
+          });
+        }}
+      >
+        Sign in with Google
+      </button>
+    );
+  } else {
+    return (
+      <button
+        onClick={async () => {
+          console.log("sign out");
+          await supabase.auth.signOut();
+          console.log("signed out");
+        }}
+      >
+        Sign Out
+      </button>
+    );
+  }
 }
