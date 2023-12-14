c611818308e8af8c70f74606d99240debaf4062a
Caleb Rivera <calebrossr@gmail.com>
2023-12-13 22:55:06 -0500

    make get all an admin procedure and add register procedure

--- packages/api/src/routers/users.ts
+++ packages/api/src/routers/users.ts
@@ -1,7 +1,23 @@
-import { privateProcedure, router } from "../trpc";
+import { insertUserSchema, users } from "db/schemas";
+import { adminProcedure, publicProcedure, router } from "../trpc";
+import { eq } from "db";
 
 export const usersRouter = router({
-  getAll: privateProcedure.query(async ({ ctx }) => {
+  getAll: adminProcedure.query(async ({ ctx }) => {
     return await ctx.db.query.users.findMany();
   }),
+  register: publicProcedure
+    .input(insertUserSchema)
+    .mutation(async ({ ctx, input }) => {
+      // If the user already exists, throw an error
+      if (input.email) {
+        const user = await ctx.db.query.users.findFirst({
+          where: eq(users.email, input.email),
+        });
+        if (user) {
+          throw new Error("User already exists");
+        }
+      }
+      return await ctx.db.insert(users).values(input);
+    }),
 });
