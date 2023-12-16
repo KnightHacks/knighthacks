4f8d46e4cf25df8a368245e96f9a5f76d3c219ed
Caleb Rivera <calebrossr@gmail.com>
2023-12-16 03:01:32 -0500

    refactor sign-in page to handle session and redirect

--- apps/admin/src/pages/SignIn.tsx
+++ apps/admin/src/pages/SignIn.tsx
@@ -1,32 +1,26 @@
-import { supabase } from "@/lib/supabase";
 import { useSession } from "@/lib/hooks/useSession";
+import { supabase } from "@/lib/supabase";
+import { Redirect } from "wouter";
 
 export function SignIn() {
   const { session } = useSession();
 
-  if (!session) {
-    return (
-      <button
-        onClick={() => {
-          supabase.auth.signInWithOAuth({
-            provider: "google",
-          });
-        }}
-      >
-        Sign in with Google
-      </button>
-    );
-  } else {
-    return (
-      <button
-        onClick={async () => {
-          console.log("sign out");
-          await supabase.auth.signOut();
-          console.log("signed out");
-        }}
-      >
-        Sign Out
-      </button>
-    );
+  if (session) {
+    return <Redirect to="/" />;
   }
+
+  return (
+    <button
+      onClick={() => {
+        supabase.auth.signInWithOAuth({
+          provider: "google",
+          options: {
+            redirectTo: "/",
+          },
+        });
+      }}
+    >
+      Sign in with Google
+    </button>
+  );
 }
