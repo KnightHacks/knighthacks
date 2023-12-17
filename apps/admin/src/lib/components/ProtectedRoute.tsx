f30e1eab7887ac849c858a677658c82776819bf4
Caleb Rivera <calebrossr@gmail.com>
2023-12-17 08:30:57 -0500

    add loading state to ProtectedRoute

--- apps/admin/src/lib/components/ProtectedRoute.tsx
+++ apps/admin/src/lib/components/ProtectedRoute.tsx
@@ -2,7 +2,11 @@ import { Redirect, Route, RouteProps } from "wouter";
 import { useSession } from "../hooks/useSession";
 
 export function ProtectedRoute(props: RouteProps) {
-  const { session } = useSession();
+  const { session, isFetchingSession } = useSession();
+
+  if (isFetchingSession) {
+    return <div>Loading...</div>;
+  }
 
   if (!session) {
     return <Redirect to="/signin" />;
