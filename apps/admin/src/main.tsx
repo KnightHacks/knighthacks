31a9b29172b53deb19ead79448b4b84d1e670982
Caleb Rivera <calebrossr@gmail.com>
2023-12-18 04:35:56 -0500

    refactor react import in main

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.tsx";
 import "./index.css";
 import "@fontsource-variable/inter";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
