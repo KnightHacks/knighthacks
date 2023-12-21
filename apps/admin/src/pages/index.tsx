c17ade4209476d40a408160503eb84748c283044
Caleb Rivera <calebrossr@gmail.com>
2023-12-21 10:58:12 -0500

    fix routing

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Route, Router, Switch } from "wouter";
+import { Route, Switch } from "wouter";
 
 import { ProtectedRoute } from "~/components/protected-route";
 import { WithNav } from "../components/with-nav";
@@ -7,25 +7,26 @@ import { HackathonRegistration } from "./hackathon-registration";
 import { HackathonSignIn } from "./hackathon-signin";
 import { Hello } from "./hello";
 import { Overview } from "./overview";
+import { SignIn } from "./sign-in";
 import { Users } from "./users";
 
 export function Pages() {
   return (
     <Switch>
       <ProtectedRoute path="/" component={WithNav(Overview)} />
-      <ProtectedRoute path="/hello" component={WithNav(Hello)} />
+      <Route path="/hello" component={WithNav(Hello)} />
+      <Route path="/signin" component={SignIn} />
       <ProtectedRoute path="/users" component={WithNav(Users)} />
-      <Router base="/hackathon">
-        <Route path="/signin" component={WithNav(HackathonSignIn)} />
-        <ProtectedRoute
-          path="/account-registration"
-          component={WithNav(HackathonAccountRegistration)}
-        />
-        <ProtectedRoute
-          path="/registration"
-          component={WithNav(HackathonRegistration)}
-        />
-      </Router>
+      <Route path="/hackathon/signin" component={WithNav(HackathonSignIn)} />
+      <Route path="/hackathon/signin" component={WithNav(HackathonSignIn)} />
+      <ProtectedRoute
+        path="/hackathon/account-registration"
+        component={WithNav(HackathonAccountRegistration)}
+      />
+      <ProtectedRoute
+        path="/hackathon/registration"
+        component={WithNav(HackathonRegistration)}
+      />
       <Route>404, Not Found!</Route>
     </Switch>
   );
