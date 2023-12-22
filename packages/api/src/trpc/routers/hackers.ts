8864b327fe1e4e2f74490874f6930af65b6cd882
Caleb Rivera <calebrossr@gmail.com>
2023-12-21 20:07:07 -0500

    use transaction to register hacker

--- packages/api/src/trpc/routers/hackers.ts
+++ packages/api/src/trpc/routers/hackers.ts
@@ -1,12 +1,32 @@
-import { hackers, insertHackerSchema } from "@knighthacks/db";
+import {
+  asc,
+  hackathons,
+  hackers,
+  insertHackerRequestSchema,
+} from "@knighthacks/db";
 
 import { router } from "../init";
 import { authenticatedProcedure } from "../procedures";
 
 export const hackersRouter = router({
   register: authenticatedProcedure
-    .input(insertHackerSchema)
-    .query(async ({ ctx, input }) => {
-      return ctx.db.insert(hackers).values(input);
+    .input(insertHackerRequestSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.transaction(async (tx) => {
+        const hackathon = await tx.query.hackathons.findFirst({
+          orderBy: [asc(hackathons.startDate)],
+        });
+
+        if (!hackathon) {
+          tx.rollback();
+          return;
+        }
+
+        await tx.insert(hackers).values({
+          ...input,
+          hackathonId: hackathon.id,
+          userId: ctx.user.id,
+        });
+      });
     }),
 });
