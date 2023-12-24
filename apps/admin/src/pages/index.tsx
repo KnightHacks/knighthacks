42b9ca332753583bf3aabd6b7e1b194f9cb6edc8
Caleb Rivera <calebrossr@gmail.com>
2023-12-24 09:28:34 -0500

    add toaster component to root

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Route, Switch } from "wouter";
 
 import { AdminRoute } from "~/components/admin-route";
 import { AuthenticatedRoute } from "~/components/authenticated-route";
+import { Toaster } from "~/components/ui/toaster.jsx";
 import { WithNav } from "~/components/with-nav.jsx";
 import { HackathonAccountRegistration } from "./hackathon-account-registration.tsx";
 import { HackathonRegistration } from "./hackathon-registration";
@@ -13,21 +14,24 @@ import { Users } from "./users";
 
 export function Pages() {
   return (
-    <Switch>
-      <AdminRoute path="/" component={WithNav(Overview)} />
-      <AdminRoute path="/users" component={WithNav(Users)} />
-      <Route path="/hello" component={WithNav(Hello)} />
-      <Route path="/signin" component={SignIn} />
-      <Route path="/hackathon/signin" component={WithNav(HackathonSignIn)} />
-      <AuthenticatedRoute
-        path="/hackathon/account-registration"
-        component={WithNav(HackathonAccountRegistration)}
-      />
-      <AuthenticatedRoute
-        path="/hackathon/registration"
-        component={WithNav(HackathonRegistration)}
-      />
-      <Route>404, Not Found!</Route>
-    </Switch>
+    <>
+      <Switch>
+        <AdminRoute path="/" component={WithNav(Overview)} />
+        <AdminRoute path="/users" component={WithNav(Users)} />
+        <Route path="/hello" component={WithNav(Hello)} />
+        <Route path="/signin" component={SignIn} />
+        <Route path="/hackathon/signin" component={HackathonSignIn} />
+        <AuthenticatedRoute
+          path="/hackathon/account-registration"
+          component={WithNav(HackathonAccountRegistration)}
+        />
+        <AuthenticatedRoute
+          path="/hackathon/registration"
+          component={WithNav(HackathonRegistration)}
+        />
+        <Route>404, Not Found!</Route>
+      </Switch>
+      <Toaster />
+    </>
   );
 }
