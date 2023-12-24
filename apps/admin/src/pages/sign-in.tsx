d26178eb9da2e57fa3417256398bf840f95249b9
Caleb Rivera <calebrossr@gmail.com>
2023-12-24 09:28:26 -0500

    update normal sign in page

--- apps/admin/src/pages/sign-in.tsx
+++ apps/admin/src/pages/sign-in.tsx
@@ -1,3 +1,15 @@
+import { SignIn as ClerkSignIn, useClerk } from "@clerk/clerk-react";
+
+import { Skeleton } from "~/components/ui/skeleton";
+
 export function SignIn() {
-  return <></>;
+  const { loaded } = useClerk();
+
+  const form = loaded ? (
+    <ClerkSignIn />
+  ) : (
+    <Skeleton className="h-[464.25px] w-[400px] rounded-2xl" />
+  );
+
+  return <div className="flex flex-1 items-center justify-center">{form}</div>;
 }
