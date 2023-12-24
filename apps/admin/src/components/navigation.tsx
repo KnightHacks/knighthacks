dca3302178c56be9dafb6d56c127ba78baf61cc9
Caleb Rivera <calebrossr@gmail.com>
2023-12-24 09:24:02 -0500

    update navigation component

--- apps/admin/src/components/navigation.tsx
+++ apps/admin/src/components/navigation.tsx
@@ -1,30 +1,73 @@
-import { SignOutButton, useAuth } from "@clerk/clerk-react";
+import { UserButton, useUser } from "@clerk/clerk-react";
 import { Link } from "wouter";
 
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { Skeleton } from "./ui/skeleton";
+import { ThemeToggle } from "./ui/theme-toggle";
+
 export function Navigation() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useUser();
+
+  const profile = isSignedIn ? (
+    <UserButton />
+  ) : isLoaded ? (
+    <Link href="/signin">Sign In</Link>
+  ) : (
+    <Skeleton className="h-8 w-32 rounded-md" />
+  );
 
   return (
-    <div className="m-4">
-      <ul>
-        <li>
-          <Link href="/">Overview</Link>
-        </li>
-        <li>
-          <Link href="/users">Users</Link>
-        </li>
-        <li>
-          <Link href="/hackers">Hackers</Link>
-        </li>
-        <li>
-          <Link href="/hackathons">Hackathons</Link>
-        </li>
-        {isSignedIn && (
-          <li>
-            <SignOutButton />
-          </li>
-        )}
-      </ul>
+    <div className="flex items-center justify-between p-4">
+      <NavigationMenu>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <Link href="/">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Overview
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/users">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Users
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/sponsors">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Sponsors
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/hackathons">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Hackathons
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <Link href="/hackers">
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                Hackers
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        {profile}
+      </div>
     </div>
   );
 }
