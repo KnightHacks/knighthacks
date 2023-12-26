1dcae5947d75e8a4aeb465bea48491eb0bfc722e
Caleb Rivera <calebrossr@gmail.com>
2023-12-26 09:53:27 -0500

    add new loading state

--- apps/admin/src/components/authenticated-route.tsx
+++ apps/admin/src/components/authenticated-route.tsx
@@ -2,11 +2,13 @@ import type { RouteProps } from "wouter";
 import { useAuth } from "@clerk/clerk-react";
 import { Redirect, Route } from "wouter";
 
+import { LoadingOverlay } from "./loading-overlay";
+
 export function AuthenticatedRoute(props: RouteProps) {
   const { isLoaded, isSignedIn } = useAuth();
 
   if (!isLoaded) {
-    return <>Loading...</>;
+    return <LoadingOverlay />;
   }
 
   if (!isSignedIn) {
