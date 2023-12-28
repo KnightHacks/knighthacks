e12c026567e3f4286110e485f6b5c00a5606686f
Caleb Rivera <calebrossr@gmail.com>
2023-12-27 22:11:52 -0500

    add db calls to webhook route

--- packages/api/src/routes/webhook.ts
+++ packages/api/src/routes/webhook.ts
@@ -1,10 +1,13 @@
-import type { WebhookEvent } from "@clerk/backend";
+import type { EmailAddressJSON, WebhookEvent } from "@clerk/backend";
 import { Hono } from "hono";
 import { Webhook } from "svix";
+
+import { eq, users } from "@knighthacks/db";
 
 import type { HonoConfig } from "../config";
 
 export const webhook = new Hono<HonoConfig>().post("/", async (c) => {
+  const db = c.get("db");
   const payload = await c.req.text();
   const id = c.req.header("svix-id");
   const timestamp = c.req.header("svix-timestamp");
@@ -23,12 +26,47 @@ export const webhook = new Hono<HonoConfig>().post("/", async (c) => {
 
   switch (event.type) {
     case "user.created":
-      break;
+      await db.insert(users).values({
+        id: event.data.id,
+        firstName: event.data.first_name,
+        lastName: event.data.last_name,
+        email: getPrimaryEmail(
+          event.data.email_addresses,
+          event.data.primary_email_address_id,
+        ),
+      });
+      return c.text(`User with id ${event.data.id} created`, 200);    
     case "user.updated":
-      break;
+      await db
+        .update(users)
+        .set({
+          firstName: event.data.first_name,
+          lastName: event.data.last_name,
+          email: getPrimaryEmail(
+            event.data.email_addresses,
+            event.data.primary_email_address_id,
+          ),
+        })
+        .where(eq(users.id, event.data.id));
+      return c.text(`User with id ${event.data.id} deleted`, 200);
     case "user.deleted":
-      break;
+      if (!event.data.deleted) return c.text("User not deleted", 400);
+      if (!event.data.id) return c.text("Missing user id", 400);
+      await db.delete(users).where(eq(users.id, event.data.id));
+      return c.text(`User with id ${event.data.id} deleted`, 200);
+    default:
+      return c.text("Invalid event type", 400);
+  }
+});
+
+function getPrimaryEmail(emails: EmailAddressJSON[], primaryEmailId: string) {
+  const primaryEmail = emails.find((e) => e.id === primaryEmailId)
+    ?.email_address;
+
+  // This in theory should never happen since users need to provide an email address to sign up
+  if (!primaryEmail) {
+    throw new Error("No primary email found");
   }
 
-  return c.text("ok", 200);
-});
+  return primaryEmail;
+}
