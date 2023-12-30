04020be1b2a9cf002e4db9f53a8d994fb20103c0
Caleb Rivera <calebrossr@gmail.com>
2023-12-30 12:24:35 -0500

    add workaround for when scrolling in popover doesnt work in portals

--- apps/admin/src/components/ui/popover.tsx
+++ apps/admin/src/components/ui/popover.tsx
@@ -20,6 +20,26 @@ const PopoverContent = React.forwardRef<
         "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 w-72 rounded-md border p-4 shadow-md outline-none",
         className,
       )}
+      /**
+       * @see https://github.com/shadcn-ui/ui/issues/607
+       */
+      onWheel={(e) => {
+        e.stopPropagation();
+
+        const isScrollingDown = e.deltaY > 0;
+
+        if (isScrollingDown) {
+          // Simulate arrow down key press
+          e.currentTarget.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowDown" }),
+          );
+        } else {
+          // Simulate arrow up key press
+          e.currentTarget.dispatchEvent(
+            new KeyboardEvent("keydown", { key: "ArrowUp" }),
+          );
+        }
+      }}
       {...props}
     />
   </PopoverPrimitive.Portal>
