b8cec11617b356a9a2e5786efb8a2baa6f20537b
Caleb Rivera <calebrossr@gmail.com>
2023-12-31 06:20:09 -0500

    add upload resume util

--- apps/admin/src/utils.ts
+++ apps/admin/src/utils.ts
@@ -5,3 +5,21 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function uploadResume(resume: File, token: string) {
+  const formData = new FormData();
+  formData.append("resume", resume);
+  const res = await fetch(
+    `${import.meta.env.VITE_API_URL}/resume/upload/${resume.name}`,
+    {
+      method: "PUT",
+      body: formData,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
+  );
+
+  const { key } = (await res.json()) as { key: string };
+  return key;
+}
