ff78095ed7a52493dd83bb3d884d118d49179f9d
Caleb Rivera <calebrossr@gmail.com>
2023-12-31 06:20:32 -0500

    create add user form

---
+++ apps/admin/src/pages/users/add-user-form.tsx
@@ -0,0 +1,101 @@
+import type { SubmitHandler } from "react-hook-form";
+import type { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { insertUserSchema } from "@knighthacks/db";
+
+import { Button } from "~/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form";
+import { Input } from "~/components/ui/input";
+import { useToast } from "~/components/ui/use-toast";
+import { trpc } from "~/trpc";
+
+const insertUserFormSchema = insertUserSchema.omit({ id: true });
+
+type InsertUserFormValues = z.infer<typeof insertUserFormSchema>;
+
+export function AddUserForm() {
+  const utils = trpc.useUtils();
+  const { toast } = useToast();
+  const { mutate, isLoading } = trpc.users.add.useMutation({
+    onSuccess: async () => {
+      await utils.users.getAll.invalidate();
+      toast({
+        title: "Success!",
+        description: "User added",
+      });
+    },
+  });
+  const form = useForm<InsertUserFormValues>({
+    resolver: zodResolver(insertUserFormSchema),
+    defaultValues: {
+      firstName: "",
+      lastName: "",
+      email: "",
+    },
+  });
+
+  const onSubmit: SubmitHandler<InsertUserFormValues> = (values) => {
+    mutate(values);
+  };
+
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex flex-col justify-center space-y-6"
+      >
+        <FormField
+          control={form.control}
+          name="firstName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>First Name</FormLabel>
+              <FormControl>
+                <Input placeholder="First Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="lastName"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Last Name</FormLabel>
+              <FormControl>
+                <Input placeholder="Last Name" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" disabled={isLoading}>
+          Add User
+        </Button>
+      </form>
+    </Form>
+  );
+}
