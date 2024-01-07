5d9724e7d58f55916434d3ae68807e243f13dc57
Sean <sean23@gmail.com>
2024-01-07 18:59:54 -0500

    changed API to admin procedures

--- packages/api/src/trpc/routers/hackathons.ts
+++ packages/api/src/trpc/routers/hackathons.ts
@@ -1,9 +1,8 @@
 import { asc, eq, hackathons, insertHackathonSchema } from "@knighthacks/db";
 
 import { router } from "../init";
-import { authenticatedProcedure, publicProcedure } from "../procedures";
+import { adminProcedure, publicProcedure } from "../procedures";
 import { z } from "zod";
-import { db } from "../../middlewares";
 
 export const hackathonsRouter = router({
   getCurrentHackathon: publicProcedure.query(async ({ ctx }) => {
@@ -15,26 +14,26 @@ export const hackathonsRouter = router({
     return hackathon ?? null;
   }),
 
-  createHackathon: authenticatedProcedure.input(insertHackathonSchema.omit({id: true})).mutation(async ({ctx, input}) => {
+  createHackathon: adminProcedure.input(insertHackathonSchema.omit({id: true})).mutation(async ({ctx, input}) => {
     console.log("Create Hackathon Input: " + input)
     const newHackathon =  await ctx.db.insert(hackathons).values(input)
 
     return newHackathon ?? null;
   }),
 
-  getAll: authenticatedProcedure.query(async ({ ctx }) => {
+  getAll: adminProcedure.query(async ({ ctx }) => {
     const getHackathons = ctx.db.query.hackathons.findMany();
 
     return getHackathons ?? null
   }),
 
-  deleteHackathon: authenticatedProcedure.input(z.string()).mutation(async ({ctx, input}) => {
+  deleteHackathon: adminProcedure.input(z.string()).mutation(async ({ctx, input}) => {
     await ctx.db.transaction(async (db) => {
     return await db.delete(hackathons).where(eq(hackathons.name, input))
     })
   }),
 
-  updateHackathon: authenticatedProcedure.input(insertHackathonSchema).mutation(async ({ctx, input}) => {
+  updateHackathon: adminProcedure.input(insertHackathonSchema).mutation(async ({ctx, input}) => {
     await ctx.db.transaction(async (db) => {
       return await db.update(hackathons).set(input).where(eq(hackathons.name, input.name))
     })
