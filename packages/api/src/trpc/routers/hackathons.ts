830ab59f2bfd29a7b360bc1dd0763e579f08e6c0
Caleb Rivera <calebrossr@gmail.com>
2024-01-10 15:44:01 -0500

    format

--- packages/api/src/trpc/routers/hackathons.ts
+++ packages/api/src/trpc/routers/hackathons.ts
@@ -1,8 +1,9 @@
+import { z } from "zod";
+
 import { asc, eq, hackathons, insertHackathonSchema } from "@knighthacks/db";
 
 import { router } from "../init";
 import { adminProcedure, publicProcedure } from "../procedures";
-import { z } from "zod";
 
 export const hackathonsRouter = router({
   getCurrentHackathon: publicProcedure.query(async ({ ctx }) => {
@@ -13,27 +14,33 @@ export const hackathonsRouter = router({
 
     return hackathon ?? null;
   }),
-
-  createHackathon: adminProcedure.input(insertHackathonSchema.omit({id: true})).mutation(async ({ctx, input}) => {
-    console.log("Create Hackathon Input: " + input)
-    const newHackathon =  await ctx.db.insert(hackathons).values(input)
-
-    return newHackathon ?? null;
-  }),
+  createHackathon: adminProcedure
+    .input(insertHackathonSchema.omit({ id: true }))
+    .mutation(async ({ ctx, input }) => {
+      const newHackathon = await ctx.db.insert(hackathons).values(input);
+      return newHackathon ?? null;
+    }),
 
   getAll: adminProcedure.query(async ({ ctx }) => {
     const getHackathons = ctx.db.query.hackathons.findMany();
 
-    return getHackathons ?? null
+    return getHackathons ?? null;
   }),
 
-  deleteHackathon: adminProcedure.input(z.string()).mutation(async ({ctx, input}) => {
-    await ctx.db.transaction(async (db) => {
-    return await db.delete(hackathons).where(eq(hackathons.name, input))
-    })
-  }),
+  deleteHackathon: adminProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.transaction(async (db) => {
+        return await db.delete(hackathons).where(eq(hackathons.name, input));
+      });
+    }),
 
-  updateHackathon: adminProcedure.input(insertHackathonSchema).mutation(async ({ctx, input}) => {
-    return await ctx.db.update(hackathons).set(input).where(eq(hackathons.name, input.name))
-  })
+  updateHackathon: adminProcedure
+    .input(insertHackathonSchema)
+    .mutation(async ({ ctx, input }) => {
+      return await ctx.db
+        .update(hackathons)
+        .set(input)
+        .where(eq(hackathons.name, input.name));
+    }),
 });
