d36787b2f64c1229117f42374ef931976a5351c4
Caleb Rivera <calebrossr@gmail.com>
2024-01-13 06:39:41 -0500

    add points to social events table

--- packages/db/src/schemas/socials.ts
+++ packages/db/src/schemas/socials.ts
@@ -4,21 +4,22 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const socialAttendees = sqliteTable("social_attendees", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   discord: text("discord").notNull(),
-  socialEventId: integer("social_event_id").references(() => socialEvent.id, {
+  socialEventId: integer("social_event_id").references(() => socialEvents.id, {
     onDelete: "cascade",
     onUpdate: "cascade",
   }),
 });
 
-export const socialEvent = sqliteTable("social_events", {
+export const socialEvents = sqliteTable("social_events", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
   description: text("description").notNull(),
   date: text("date").notNull(),
   location: text("location").notNull(),
+  points: integer("points").notNull(),
 });
 
-export const socialEventRelations = relations(socialEvent, ({ many }) => {
+export const socialEventsRelations = relations(socialEvents, ({ many }) => {
   return {
     attendees: many(socialAttendees),
   };
