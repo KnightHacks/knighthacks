0f5e560a8df191bfbea2054393737f319aae78bb
Caleb Rivera <calebrossr@gmail.com>
2024-01-13 07:08:45 -0500

    update socials schema

--- packages/db/src/schemas/socials.ts
+++ packages/db/src/schemas/socials.ts
@@ -4,23 +4,50 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 export const socialAttendees = sqliteTable("social_attendees", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   discord: text("discord").notNull(),
-  socialEventId: integer("social_event_id").references(() => socialEvents.id, {
-    onDelete: "cascade",
-    onUpdate: "cascade",
-  }),
 });
+
+export const socialAttendeesRelations = relations(
+  socialAttendees,
+  ({ many }) => ({
+    socialAttendeesToSocialEvents: many(socialAttendeesToSocialEvents),
+  }),
+);
 
 export const socialEvents = sqliteTable("social_events", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
-  description: text("description").notNull(),
   date: text("date").notNull(),
   location: text("location").notNull(),
   points: integer("points").notNull(),
 });
 
-export const socialEventsRelations = relations(socialEvents, ({ many }) => {
-  return {
-    attendees: many(socialAttendees),
-  };
-});
+export const socialEventsRelations = relations(socialAttendees, ({ many }) => ({
+  socialAttendeesToSocialEvents: many(socialAttendeesToSocialEvents),
+}));
+
+export const socialAttendeesToSocialEvents = sqliteTable(
+  "social_attendee_events",
+  {
+    id: integer("id").primaryKey({ autoIncrement: true }),
+    attendeeId: integer("attendee_id")
+      .notNull()
+      .references(() => socialAttendees.id),
+    eventId: integer("event_id")
+      .notNull()
+      .references(() => socialEvents.id),
+  },
+);
+
+export const socialAttendeesToSocialEventsRelations = relations(
+  socialAttendeesToSocialEvents,
+  ({ one }) => ({
+    socialAttendees: one(socialAttendees, {
+      fields: [socialAttendeesToSocialEvents.attendeeId],
+      references: [socialAttendees.id],
+    }),
+    socialEvents: one(socialEvents, {
+      fields: [socialAttendeesToSocialEvents.eventId],
+      references: [socialEvents.id],
+    }),
+  }),
+);
