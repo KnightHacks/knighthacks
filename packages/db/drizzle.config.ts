10114cce71752438bcd109ff81e4e78b35015802
Caleb Rivera <calebrossr@gmail.com>
2024-01-13 06:39:30 -0500

    update schemas in config

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./migrations",
-  schema: ["./src/schemas/schema.ts"],
+  schema: ["./src/schemas/hackathon.ts", "./src/schemas/socials.ts"],
   driver: "turso",
   dbCredentials: {
     url: process.env.TURSO_URL!,
