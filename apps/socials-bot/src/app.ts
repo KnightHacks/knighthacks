372530b7f10de9549a9675dc774f8ad91e667bf0
Caleb Rivera <calebrossr@gmail.com>
2024-01-14 21:52:25 -0500

    remove eslint disable comments

--- apps/socials-bot/src/app.ts
+++ apps/socials-bot/src/app.ts
@@ -1,9 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-argument */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-
 // I hate Discord's typings
 
 import type { Next } from "hono";
