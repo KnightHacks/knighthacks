7eae206088ecab82c1f0d5775936a36eb7f11346
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 16:45:31 -0500

    remove unused imports

--- apps/socials-bot/src/app.ts
+++ apps/socials-bot/src/app.ts
@@ -1,24 +1,11 @@
-// I hate Discord's typings
-
-import type { Next } from "hono";
-import {
-  InteractionResponseType,
-  InteractionType,
-  verifyKey,
-} from "discord-interactions";
 import { Hono } from "hono";
 
-import { connect } from "@knighthacks/db";
-
-import type { HonoConfig, HonoContext } from "~/config";
-import { signIn } from "./commands/sign-in";
-import { verifyDiscordRequest } from "./middlewares/verify-discord-request";
+import type { HonoConfig } from "~/config";
 import { helloWorld } from "./routes/hello-world";
 import { interaction } from "./routes/interaction";
 
 const app = new Hono<HonoConfig>()
   .route("/", helloWorld)
-  .route('/', interaction)
-  
+  .route("/", interaction);
 
 export { app };
