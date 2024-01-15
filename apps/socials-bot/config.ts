f8310cf72aa1ca51f85533cbc267ceb0c8e454e7
Caleb Rivera <calebrossr@gmail.com>
2024-01-14 21:36:48 -0500

    update config for bot

--- apps/socials-bot/config.ts
+++ apps/socials-bot/config.ts
@@ -1,6 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/consistent-type-definitions */
 
-import type { InteractionType } from "discord-interactions";
 import type { Context, Input } from "hono";
 
 import type { connect } from "@knighthacks/db";
@@ -11,20 +11,12 @@ export type Bindings = {
   DISCORD_BOT_TOKEN: string;
   DISCORD_CLIENT_ID: string;
   DISCORD_PUBLIC_KEY: string;
+  DISCORD_ADMIN_ROLE_ID: string;
 };
-
-export interface Interaction {
-  [key: string]: unknown;
-  type: InteractionType;
-  data: {
-    [key: string]: unknown;
-    name: string;
-  };
-}
 
 export type Variables = {
   db: ReturnType<typeof connect>;
-  interaction: Interaction;
+  interaction: any; // TODO: type this
 };
 
 export type HonoConfig = {
