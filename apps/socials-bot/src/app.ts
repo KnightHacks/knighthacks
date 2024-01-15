13e231e5c693e8ba01567c0c385ed46d97eac155
Caleb Rivera <calebrossr@gmail.com>
2024-01-14 21:37:37 -0500

    handle createevent command

--- apps/socials-bot/src/app.ts
+++ apps/socials-bot/src/app.ts
@@ -1,3 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+
+// I hate Discord's typings
+
 import type { Next } from "hono";
 import {
   InteractionResponseType,
@@ -6,8 +14,9 @@ import {
 } from "discord-interactions";
 import { Hono } from "hono";
 
-import type { HonoConfig, HonoContext, Interaction } from "../config";
-import type { Command } from "./commands";
+import { connect, socialEvents } from "@knighthacks/db";
+
+import type { HonoConfig, HonoContext } from "../config";
 
 async function verifyDiscordRequest(c: HonoContext, next: Next) {
   const signature = c.req.header("x-signature-ed25519");
@@ -26,36 +35,70 @@ async function verifyDiscordRequest(c: HonoContext, next: Next) {
     return c.text("Invalid request signature", 401);
   }
 
-  c.set("interaction", JSON.parse(text) as Interaction);
+  c.set("interaction", JSON.parse(text));
 
   return next();
 }
 
-const app = new Hono<HonoConfig>().post("/", verifyDiscordRequest, (c) => {
-  const interaction = c.get("interaction");
+function isAdmin(roles: string[], adminRoleId: string) {
+  return roles.includes(adminRoleId);
+}
 
-  if (interaction.type === InteractionType.PING) {
-    return c.json({
-      type: InteractionResponseType.PONG,
-    });
-  }
+const app = new Hono<HonoConfig>().post(
+  "/",
+  verifyDiscordRequest,
+  async (c) => {
+    const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+    const interaction = c.get("interaction");
 
-  if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-    const command = interaction.data.name as Command["name"];
-    switch (command) {
-      case "ping":
-        return c.json({
-          type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-          data: {
-            content: "pong!",
-          },
-        });
-      default:
-        return c.text("Unknown command", 400);
+    if (interaction.type === InteractionType.PING) {
+      return c.json({
+        type: InteractionResponseType.PONG,
+      });
     }
-  }
 
-  return c.text("Unknown interaction type", 400);
-});
+    if (interaction.type === InteractionType.APPLICATION_COMMAND) {
+      const command = interaction.data;
+      switch (command.name) {
+        case "ping": {
+          return c.json({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: "pong!",
+            },
+          });
+        }
+        case "createevent": {
+          if (
+            isAdmin(
+              interaction.member.roles as string[],
+              c.env.DISCORD_ADMIN_ROLE_ID,
+            )
+          ) {
+            return c.text("You are not an admin!", 403);
+          }
+          const socialEvent = command.options.reduce((socialEvent, item) => {
+            socialEvent[item.name] = item.value;
+            return socialEvent;
+          }, {});
+          await db.insert(socialEvents).values(socialEvent);
+          return c.json({
+            type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+            data: {
+              content: `Created new event ${socialEvent.name} on ${socialEvent.date} at ${socialEvent.location} worth ${socialEvent.points} points!`,
+            },
+          });
+        }
+        case "signin": {
+          return;
+        }
+        default:
+          return c.text("Unknown command", 400);
+      }
+    }
+
+    return c.text("Unknown interaction type", 400);
+  },
+);
 
 export { app };
