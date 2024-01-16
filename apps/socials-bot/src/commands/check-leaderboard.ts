ceb4516332474a79c213bde365e319631233556c
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 19:52:46 -0500

    add check leaderboard command

---
+++ apps/socials-bot/src/commands/check-leaderboard.ts
@@ -0,0 +1,38 @@
+import { connect } from "@knighthacks/db";
+
+export async function checkLeaderboard(db: ReturnType<typeof connect>) {
+  // TODO: find a more efficient way get attendees sorted by points
+
+  // Get all attendees
+  const attendees = await db.query.attendees.findMany({
+    with: {
+      attendeesEvents: {
+        with: {
+          events: true,
+        },
+      },
+    },
+  });
+
+  if (!attendees.length) {
+    throw new Error("No attendees found!");
+  }
+
+  const attendeesWithPoints = attendees.map((attendee) => {
+    const points = attendee.attendeesEvents.reduce((acc, curr) => {
+      return acc + curr.events.points;
+    }, 0);
+
+    return {
+      ...attendee,
+      points,
+    };
+  });
+
+  // Sort attendees by points
+  const sortedAttendees = attendeesWithPoints.sort((a, b) => {
+    return b.points - a.points;
+  });
+
+  return sortedAttendees;
+}
