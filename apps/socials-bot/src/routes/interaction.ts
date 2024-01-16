3fc7fc5cdb36dbe261b81abffa40d7044999dcbd
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 19:52:54 -0500

    register all commands

--- apps/socials-bot/src/routes/interaction.ts
+++ apps/socials-bot/src/routes/interaction.ts
@@ -1,11 +1,17 @@
 import { InteractionResponseType, InteractionType } from "discord-interactions";
 import { Hono } from "hono";
 
-import { connect } from "@knighthacks/db";
+import { connect, events } from "@knighthacks/db";
 
+import { Command } from "~/commands";
+import { checkLeaderboard } from "~/commands/check-leaderboard";
+import { checkPoints } from "~/commands/check-points";
+import { clearEvents } from "~/commands/clear-events";
+import { createEvent } from "~/commands/create-event";
 import { signIn } from "~/commands/sign-in";
 import { HonoConfig } from "~/config";
 import { verifyDiscordRequest } from "~/middlewares/verify-discord-request";
+import { getUserPermissions } from "~/utils";
 
 export const interaction = new Hono<HonoConfig>().post(
   "/",
@@ -20,27 +26,119 @@ export const interaction = new Hono<HonoConfig>().post(
     }
 
     if (interaction.type === InteractionType.APPLICATION_COMMAND) {
-      let commandName = interaction.data.name;
+      let commandName = interaction.data.name as Command["name"];
       switch (commandName) {
-        case "ping": {
+        case "ping":
           return c.json({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
-              content: "pong!",
+              content: "Pong!",
             },
           });
-        }
         case "createevent": {
+          try {
+            const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+            const permissions = getUserPermissions(interaction.member.roles);
+            const event = await createEvent(interaction, db, permissions);
+            return c.json({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `Created new event ${event.name} on ${event.date} at ${event.location} worth ${event.points} points!`,
+              },
+            });
+          } catch (error) {
+            if (error instanceof Error) {
+              return c.json({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content: error.message,
+                },
+              });
+            }
+          }
         }
         case "signin": {
+          try {
+            const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+            await signIn(interaction, db);
+            return c.json({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: "Signed you in!",
+              },
+            });
+          } catch (error) {
+            if (error instanceof Error) {
+              return c.json({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content: error.message,
+                },
+              });
+            }
+          }
+        }
+        case "checkpoints": {
           const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
-          await signIn(c, interaction, db);
+          const points = await checkPoints(interaction, db);
           return c.json({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             data: {
-              content: "Signed you in!",
+              content: `You have ${points} points!`,
             },
           });
+        }
+        case "clearevents": {
+          try {
+            const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+            const permissions = getUserPermissions(interaction.member.roles);
+            await clearEvents(db, permissions);
+            return c.json({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: "Cleared all events!",
+              },
+            });
+          } catch (error) {
+            if (error instanceof Error) {
+              return c.json({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content: error.message,
+                },
+              });
+            }
+          }
+        }
+        case "checkleaderboard": {
+          try {
+            const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+            const leaderboard = await checkLeaderboard(db);
+            const leaderboardString = leaderboard
+              .map(
+                (attendee, index) =>
+                  `${index + 1}. <@${attendee.discord}> - ${
+                    attendee.points
+                  } points`,
+              )
+              .join("\n");
+
+            return c.json({
+              type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+              data: {
+                content: `**Leaderboard**\n${leaderboardString}`,
+              },
+            });
+          } catch (error) {
+            if (error instanceof Error) {
+              return c.json({
+                type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                data: {
+                  content: error.message,
+                },
+              });
+            }
+          }
         }
         default:
           return c.text("Unknown command", 400);
