0d43ff7305af4c84910623081db467b6f5d7e09d
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 22:06:35 -0500

    add dev lead role

--- apps/socials-bot/src/utils.ts
+++ apps/socials-bot/src/utils.ts
@@ -1,8 +1,13 @@
 export const PRESIDENT_ROLE_ID = "486629323797692431";
 export const VICE_PRESIDENT_ROLE_ID = "623165798151618591";
 export const OFFICER_ROLE_ID = "486629374758748180";
+export const DEV_LEAD_ROLE_ID = "1164631876024143872";
 
-export type Permission = "president" | "vice-president" | "officer";
+export type Permission =
+  | "president"
+  | "vice-president"
+  | "officer"
+  | "dev-lead";
 
 export function getUserPermissions(roles: string[]) {
   return roles.reduce((roles, role) => {
@@ -12,6 +17,8 @@ export function getUserPermissions(roles: string[]) {
       roles.push("vice-president");
     } else if (role === OFFICER_ROLE_ID) {
       roles.push("officer");
+    } else if (role === DEV_LEAD_ROLE_ID) {
+      roles.push("dev-lead");
     }
     return roles;
   }, [] as Permission[]);
