6f865605bc5105dcb2a7d1c98460faaa03c7b207
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 19:04:00 -0500

    uncomment checking for permissions

--- apps/socials-bot/src/commands/clear-events.ts
+++ apps/socials-bot/src/commands/clear-events.ts
@@ -6,13 +6,13 @@ export async function clearEvents(
   db: ReturnType<typeof connect>,
   permissions: Permission[],
 ) {
-  // if (
-  //   !permissions.includes("president") ||
-  //   !permissions.includes("vice-president")
-  // )
-  //   throw new Error(
-  //     "You must be the president or vice president to clear events",
-  //   );
+  if (
+    !permissions.includes("president") ||
+    !permissions.includes("vice-president")
+  )
+    throw new Error(
+      "You must be the president or vice president to clear events",
+    );
 
   return await db.delete(events);
 }
