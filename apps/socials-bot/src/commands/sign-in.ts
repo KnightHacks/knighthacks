a848b1588ddeaacb74e0aaac1a5c5c638fdb4b1d
Caleb Rivera <calebrossr@gmail.com>
2024-01-15 19:00:10 -0500

    add sign in command

--- apps/socials-bot/src/commands/sign-in.ts
+++ apps/socials-bot/src/commands/sign-in.ts
@@ -1,10 +1,73 @@
-import { connect } from "@knighthacks/db";
+import {
+  attendees,
+  attendeesEvents,
+  connect,
+  eq,
+  events,
+} from "@knighthacks/db";
 
-import { HonoContext } from "~/config";
-import { isAdmin } from "../utils";
+export async function signIn(interaction: any, db: ReturnType<typeof connect>) {
+  const { id: discordId } = interaction.member.user;
+  const secret = interaction.data.options[0].value;
 
-export async function signIn(
-  c: HonoContext,
-  interaction: any,
-  db: ReturnType<typeof connect>,
-) {}
+  // Check if event exists
+  let event = await db.query.events.findFirst({
+    where: eq(events.secret, secret),
+  });
+
+  if (!event) {
+    throw new Error("Event does not exist");
+  }
+
+  // Check if attendee exists
+  let attendee = await db.query.attendees.findFirst({
+    where: eq(attendees.discord, discordId),
+    with: {
+      attendeesEvents: {
+        with: {
+          events: true,
+        },
+      },
+    },
+    columns: {
+      id: true,
+    },
+  });
+
+  // Handle case where attendee does not exist
+  if (!attendee) {
+    let insertedAttendee = (
+      await db
+        .insert(attendees)
+        .values({
+          discord: discordId,
+        })
+        .returning({ id: attendees.id })
+    )[0];
+    if (!insertedAttendee) {
+      throw new Error("Failed to insert attendee");
+    }
+    // Check in attendee to event
+    await db.insert(attendeesEvents).values({
+      attendeeId: insertedAttendee.id,
+      eventId: event.id,
+    });
+
+    return;
+  }
+
+  // Check if they have already signed into this event
+  if (
+    attendee?.attendeesEvents.find(
+      (attendeeEvent) => attendeeEvent.events.secret === secret,
+    )
+  ) {
+    throw new Error("You have already signed into this event");
+  }
+
+  // Check in attendee to event
+  await db.insert(attendeesEvents).values({
+    attendeeId: attendee.id,
+    eventId: event.id,
+  });
+}
