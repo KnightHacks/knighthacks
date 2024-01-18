e3f8d650487486ec4265ebfd7eb197180ffc4adc
Caleb Rivera <calebrossr@gmail.com>
2024-01-18 18:14:06 -0500

    limit number of attendees to five

--- apps/socials-bot/src/commands/check-leaderboard.ts
+++ apps/socials-bot/src/commands/check-leaderboard.ts
@@ -5,6 +5,7 @@ export async function checkLeaderboard(db: ReturnType<typeof connect>) {
 
   // Get all attendees
   const attendees = await db.query.attendees.findMany({
+    limit: 5,
     with: {
       attendeesEvents: {
         with: {
