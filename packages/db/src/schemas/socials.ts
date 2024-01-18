4ca3cd1db47d8ef150c6d885f904123648e35c55
Caleb Rivera <calebrossr@gmail.com>
2024-01-18 16:50:46 -0500

    update attendees table to include both id and username

--- packages/db/src/schemas/socials.ts
+++ packages/db/src/schemas/socials.ts
@@ -3,7 +3,8 @@ import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const attendees = sqliteTable("attendees", {
   id: integer("id").primaryKey({ autoIncrement: true }),
-  discord: text("discord").notNull().unique(),
+  discordId: text("discord_id").notNull().unique(),
+  discordUsername: text("discord_username").unique(),
 });
 
 export const attendeesRelations = relations(attendees, ({ many }) => ({
