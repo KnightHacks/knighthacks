635172a7ec741cc64960315054c7a76c09153324
Caleb Rivera <calebrossr@gmail.com>
2024-01-24 05:06:01 -0500

    use new env vars name for db

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
   schema: ["./src/schemas/hackathon.ts", "./src/schemas/socials.ts"],
   driver: "turso",
   dbCredentials: {
-    url: process.env.TURSO_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.DATABASE_AUTH_TOKEN,
   },
 });
