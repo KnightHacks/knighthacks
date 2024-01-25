47533b0f5e5d9edce3cf69e3e159839e2520be9c
Caleb Rivera <calebrossr@gmail.com>
2024-01-25 06:07:18 -0500

    set up knight hacks 2024 site

---
+++ apps/2024/src/app/page.tsx
@@ -0,0 +1,10 @@
+import { Hello } from "~/components/Hello";
+
+export default function Home() {
+  return (
+    <div>
+      <div>This is the home page!</div>
+      <Hello />
+    </div>
+  );
+}
