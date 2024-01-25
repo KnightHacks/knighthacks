73aba24eedbb9e912abaa17428cd418e89088efc
Caleb Rivera <calebrossr@gmail.com>
2024-01-25 06:07:18 -0500

    set up knight hacks 2024 site

---
+++ apps/2024/next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /** Enables hot reloading for local packages without a build step */
+  transpilePackages: ["@knighthacks/api"],
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
+};
+
+export default nextConfig;
