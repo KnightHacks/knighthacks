ad48c0f050a0ef8b9da89bc414c0332b5d4cda41
Caleb Rivera <calebrossr@gmail.com>
2024-01-25 19:05:20 -0500

    update use of bindings

--- packages/api/src/middlewares/db.tsx
+++ packages/api/src/middlewares/db.tsx
@@ -6,7 +6,7 @@ import type { HonoContext } from "../config";
 
 // Spawn a new database connection per request and attach it to the context.
 export function db(c: HonoContext, next: Next) {
-  const db = connect(c.env.TURSO_URL, c.env.TURSO_AUTH_TOKEN);
+  const db = connect(c.env.DATABASE_URL, c.env.DATABASE_AUTH_TOKEN);
   c.set("db", db);
   return next();
 }
