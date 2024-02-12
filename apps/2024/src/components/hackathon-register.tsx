447f170a12ab7ddfee3caa29d7efefadceecfaf0
gitSean23 <the.feldman23@gmail.com>
2024-02-12 12:20:43 -0500

    added hackathon register modal

---
+++ apps/2024/src/components/hackathon-register.tsx
@@ -0,0 +1,43 @@
+import { Button } from "./ui/button"
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export function HackathonRegister() {
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline">Register for the Hackathon</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[800px]">
+        <DialogHeader>
+          <DialogTitle>Registration</DialogTitle>
+        </DialogHeader>
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="name" className="text-right">
+            Why do you want to attend Knight Hacks?
+            </Label>
+            <Input id="name" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="username" className="text-right">
+            What do you want to learn at Knight Hacks?
+            </Label>
+            <Input id="username" className="col-span-3" />
+          </div>
+        </div>
+        <DialogFooter>
+          <Button type="submit">Submit</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  )
+}
