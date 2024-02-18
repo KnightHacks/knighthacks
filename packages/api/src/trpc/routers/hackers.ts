8b39434996aa2e2e1a1ee586c76e92ee355f6a8e
Caleb Rivera <calebrossr@gmail.com>
2024-02-18 00:52:45 -0500

    add hackers procedures

--- packages/api/src/trpc/routers/hackers.ts
+++ packages/api/src/trpc/routers/hackers.ts
@@ -1,12 +1,15 @@
+import { z } from "zod";
+
 import {
   asc,
+  eq,
   hackathons,
   hackers,
   insertHackerRequestSchema,
 } from "@knighthacks/db";
 
 import { router } from "../init";
-import { authenticatedProcedure } from "../procedures";
+import { adminProcedure, authenticatedProcedure } from "../procedures";
 
 export const hackersRouter = router({
   register: authenticatedProcedure
@@ -28,5 +31,67 @@ export const hackersRouter = router({
           userId: ctx.user.id,
         });
       });
+    }),
+  getAll: adminProcedure.query(async ({ ctx }) => {
+    return ctx.db.query.hackers.findMany({
+      with: {
+        hackathon: true,
+      },
+    });
+  }),
+  getMyHackathons: authenticatedProcedure.query(async ({ ctx }) => {
+    return ctx.db.query.hackers.findMany({
+      where: eq(hackers.userId, ctx.user.id),
+      with: {
+        hackathon: true,
+      },
+    });
+  }),
+  getMyStatus: authenticatedProcedure.query(async ({ ctx }) => {
+    return await ctx.db.transaction(async (tx) => {
+      const hackathon = await tx.query.hackathons.findFirst({
+        orderBy: [asc(hackathons.startDate)],
+      });
+
+      if (!hackathon) {
+        tx.rollback();
+        return;
+      }
+
+      const hacker = await tx.query.hackers.findFirst({
+        where: eq(hackers.userId, ctx.user.id),
+      });
+
+      if (!hacker) {
+        tx.rollback();
+        return;
+      }
+
+      return hacker.status;
+    });
+  }),
+  update: authenticatedProcedure
+    .input(insertHackerRequestSchema)
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.transaction(async (tx) => {
+        const hackathon = await tx.query.hackathons.findFirst({
+          orderBy: [asc(hackathons.startDate)],
+        });
+
+        if (!hackathon) {
+          tx.rollback();
+          return;
+        }
+
+        await tx
+          .update(hackers)
+          .set(input)
+          .where(eq(hackers.userId, ctx.user.id));
+      });
+    }),
+  delete: authenticatedProcedure
+    .input(z.string())
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.delete(hackers).where(eq(hackers.userId, input));
     }),
 });
