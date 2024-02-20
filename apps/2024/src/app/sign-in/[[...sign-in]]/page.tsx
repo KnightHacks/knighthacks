b74dbe0b03caae3cfd959ef709865017d40fcc3a
Caleb Rivera <calebrossr@gmail.com>
2024-02-20 06:53:48 -0500

    add edge runtimes to non-static routes

--- apps/2024/src/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/2024/src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignIn as ClerkSignIn } from "@clerk/nextjs";
 
+export const runtime = "edge";
+
 export default function SignIn() {
   return (
     <div className="flex flex-1 items-center justify-center">
--- apps/2024/src/app/sign-up/[[...sign-up]]/page.tsx
+++ apps/2024/src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp as ClerkSignUp } from "@clerk/nextjs";
 
+export const runtime = "edge";
+
 export default function SignUp() {
   return (
     <div className="flex flex-1 items-center justify-center">
