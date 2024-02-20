1205a344b732ca84096e3f05e7ec7bb247a38536
Caleb Rivera <calebrossr@gmail.com>
2024-02-20 05:27:59 -0500

    remove default dark theme

--- apps/admin/src/components/providers.tsx
+++ apps/admin/src/components/providers.tsx
@@ -1,10 +1,10 @@
-import { ClerkProviderWithTheme } from "./clerk-provider";
+import { ClerkProviderWithTheme } from "./clerk-provider-with-theme";
 import { TRPCProvider } from "./trpc-provider";
 import { ThemeProvider } from "./ui/theme-provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider defaultTheme="dark">
+    <ThemeProvider>
       <ClerkProviderWithTheme>
         <TRPCProvider>{children}</TRPCProvider>
       </ClerkProviderWithTheme>
