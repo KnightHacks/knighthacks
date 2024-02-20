54d55a01f736a65488563c48381638aa2c77b40a
Caleb Rivera <calebrossr@gmail.com>
2024-02-20 06:33:21 -0500

    fix page directories

--- apps/2024/src/app/components/clerk-provider-with-theme.tsx
+++ apps/2024/src/app/components/clerk-provider-with-theme.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ClerkProvider } from "@clerk/nextjs";
 import { dark } from "@clerk/themes";
 import { useTheme } from "next-themes";
@@ -33,6 +35,10 @@ export function ClerkProviderWithTheme({
           footerActionText: "text-muted-foreground",
         },
       }}
+      afterSignInUrl={process.env.NEXT_PUBLIC_2024_AFTER_SIGN_IN_URL}
+      afterSignUpUrl={process.env.NEXT_PUBLIC_2024_AFTER_SIGN_UP_URL}
+      signUpUrl={process.env.NEXT_PUBLIC_2024_SIGN_UP_URL}
+      signInUrl={process.env.NEXT_PUBLIC_2024_SIGN_IN_URL}
     >
       {children}
     </ClerkProvider>
