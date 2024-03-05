e62387645a758304241f640769396b380315cdc4
gitSean23 <the.feldman23@gmail.com>
2024-03-04 20:58:14 -0500

    format fix

--- packages/design-system/src/index.ts
+++ packages/design-system/src/index.ts
@@ -7,5 +7,5 @@ export * from "./components/ui/input";
 export * from "./components/ui/label";
 export * from "./components/ui/theme-provider";
 export * from "./components/ui/theme-toggle";
-export * from './components/ui/button';
-export * from './components/ui/textarea';
+export * from "./components/ui/button";
+export * from "./components/ui/textarea";
