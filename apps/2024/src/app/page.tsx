14c67fd00fa5d44542bf4d2e27636da03504e08e
Caleb Rivera <calebrossr@gmail.com>
2024-03-05 19:35:29 -0500

    Export edge runtime in home page

--- apps/2024/src/app/page.tsx
+++ apps/2024/src/app/page.tsx
@@ -2,6 +2,8 @@ import { currentUser } from "@clerk/nextjs";
 
 import { SignOut } from "./_components/sign-out";
 
+export const runtime = "edge";
+
 export default async function Home() {
   const user = await currentUser();
 
