b77c2f56cd622bc977d9b8f8977a544d4db7a434
Caleb Rivera <calebrossr@gmail.com>
2024-03-05 19:10:49 -0500

    Fix tailwind config path

--- packages/ui/tailwind.config.ts
+++ packages/ui/tailwind.config.ts
@@ -3,7 +3,8 @@
  * for Tailwind Intellisense & Autocompletion in the source files
  */
 import type { Config } from "tailwindcss";
-import baseConfig from "@acme/tailwind-config/web";
+
+import baseConfig from "@knighthacks/tailwind-config/web";
 
 export default {
   content: ["./src/**/*.tsx"],
