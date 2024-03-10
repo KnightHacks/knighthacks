392e46c1b9617a0b8a66ee156e6a6bc787daca19
Caleb Rivera <calebrossr@gmail.com>
2024-03-10 00:34:42 -0500

    Remove sign out button on home page

--- apps/admin-new/src/app/(dashboard)/page.tsx
+++ apps/admin-new/src/app/(dashboard)/page.tsx
@@ -1,16 +1,9 @@
-import { currentUser } from "@clerk/nextjs";
-
-import { SignOut } from "../_components/sign-out";
-
 export const runtime = "edge";
 
 export default async function Home() {
-  const user = await currentUser();
-
   return (
     <div>
       <div>This is the home page!</div>
-      {user && <SignOut />}
     </div>
   );
 }
