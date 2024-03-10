5f1408b90893c83b23204ae0c0bfe490392e9d13
Caleb Rivera <calebrossr@gmail.com>
2024-03-10 00:15:21 -0500

    Add layout component

---
+++ apps/admin-new/src/app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import { Navigation } from "./navigation";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <Navigation />
+      {children}
+    </>
+  );
+}
