4e1bf415aab90497b50b26472911c21e8ac72941
Caleb Rivera <calebrossr@gmail.com>
2024-03-11 04:00:23 -0400

    Add sign out button

---
+++ apps/admin-new/src/app/_components/sign-out.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useAuth } from "@clerk/nextjs";
+
+import { Button } from "@knighthacks/ui/button";
+
+export function SignOut() {
+  const { signOut } = useAuth();
+
+  return <Button onClick={() => signOut()}>Sign Out</Button>;
+}
