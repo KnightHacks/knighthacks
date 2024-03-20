9e6e84da35fc03e9b94a75ea5924f738ee81a4c7
farbubby <farhan.mahbub@ucf.edu>
2024-03-20 18:53:59 -0400

    Add profile page and remove old profile page

---
+++ apps/2024/src/app/application/profile/page.tsx
@@ -0,0 +1,18 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
+import { ProfileForm } from "./profile-form";
+
+export default function Profile() {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <div className="mx-auto w-full max-w-3xl p-4">
+      <ProfileForm userId={userId} />
+    </div>
+  );
+}
