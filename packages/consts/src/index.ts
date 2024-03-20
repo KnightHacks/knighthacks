946c0d93bf3dbc779c5d1c607496d71f0c55b351
Caleb Rivera <calebrossr@gmail.com>
2024-03-19 20:06:08 -0400

    Export application statuses

--- packages/consts/src/index.ts
+++ packages/consts/src/index.ts
@@ -2,4 +2,5 @@ export * from "./majors";
 export * from "./schools";
 export * from "./shirt-sizes";
 export * from "./graduation-years";
-export * from "./sponsor-tiers"
+export * from "./sponsor-tiers";
+export * from "./application-statuses";
