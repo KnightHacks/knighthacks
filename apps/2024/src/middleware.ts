3f789190ed6251d6a9754d247fac494386b411b8
Caleb Rivera <calebrossr@gmail.com>
2024-04-15 06:00:19 -0400

    Update authMiddleware configuration in middleware.ts

--- apps/2024/src/middleware.ts
+++ apps/2024/src/middleware.ts
@@ -5,7 +5,6 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/"],
-  signInUrl: "/sign-in",
 });
 
 export const config = {
