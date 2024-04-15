12ed8402f616e675e2543778f56d534c5578c8fb
Caleb Rivera <calebrossr@gmail.com>
2024-04-15 06:00:25 -0400

    Add smooth scrolling

--- apps/2024/src/app/layout.tsx
+++ apps/2024/src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className="scroll-smooth">
       <body
         className={cn(
           "min-h-screen bg-background font-sans text-foreground antialiased",
