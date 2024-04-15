836e080b14fefba9ec4f9c645cefeccfd5f7ca3b
Caleb Rivera <calebrossr@gmail.com>
2024-04-15 06:00:06 -0400

    Add new sections to the main page

---
+++ apps/2024/src/app/(main)/_components/sections/about.tsx
@@ -0,0 +1,7 @@
+export function About() {
+  return (
+    <section id="about" className="flex h-screen items-center justify-center">
+      <h1>About</h1>
+    </section>
+  );
+}
---
+++ apps/2024/src/app/(main)/_components/sections/faq.tsx
@@ -0,0 +1,7 @@
+export function FAQ() {
+  return (
+    <section id="faq" className="flex h-screen items-center justify-center">
+      <h1>FAQ</h1>
+    </section>
+  );
+}
