92fb79139ec7189bec0b0333809e2ac8f82f8764
Caleb Rivera <calebrossr@gmail.com>
2024-04-16 18:59:32 -0400

    Update footer component in main layout

--- apps/2024/src/app/(main)/_components/footer.tsx
+++ apps/2024/src/app/(main)/_components/footer.tsx
@@ -1,10 +1,42 @@
+import Link from "next/link";
+
 export function Footer() {
   return (
     <footer className="p-8">
-      <ul className="flex justify-center gap-2">
-        <li>MLH Code of Conduct</li>•<li>Sponsor Us</li>•<li>Profile</li>
+      <ul className="mb-4  flex justify-center gap-2">
+        <li>
+          <a
+            className="underline underline-offset-4"
+            href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf"
+          >
+            MLH Code of Conduct
+          </a>
+        </li>
+        •
+        <li>
+          <a
+            className="underline underline-offset-4"
+            href="mailto:sponsorship@knighthacks.org"
+          >
+            Sponsor Us
+          </a>
+        </li>
+        •
+        <li>
+          <Link className="underline underline-offset-4" href="/profile">
+            Profile
+          </Link>
+        </li>
       </ul>
-      <div className="text-center">Made with ❤️ from KnightHacks</div>
+      <div className="text-center">
+        Made with ❤️{" "}
+        <a
+          className="font-bold underline underline-offset-4"
+          href="https://github.com/KnightHacks"
+        >
+          @KnightHacks
+        </a>
+      </div>
     </footer>
   );
 }
