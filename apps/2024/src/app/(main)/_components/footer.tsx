cffe74669b498cfdb1fa6756fd3c59797472edff
Caleb Rivera <calebrossr@gmail.com>
2024-04-16 14:55:13 -0400

    Add footer component to main layout

---
+++ apps/2024/src/app/(main)/_components/footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <footer className="p-8">
+      <ul className="flex justify-center gap-2">
+        <li>MLH Code of Conduct</li>•<li>Sponsor Us</li>•<li>Profile</li>
+      </ul>
+      <div className="text-center">Made with ❤️ from KnightHacks</div>
+    </footer>
+  );
+}
