f916645858588c67350af0fe224fafc36a653b14
Caleb Rivera <calebrossr@gmail.com>
2024-04-16 14:55:28 -0400

    Update hero section in main page

--- apps/2024/src/app/(main)/_components/sections/hero.tsx
+++ apps/2024/src/app/(main)/_components/sections/hero.tsx
@@ -1,7 +1,32 @@
+import Link from "next/link";
+
+import { Button } from "@knighthacks/ui/button";
+
+import { Island } from "../island";
+
 export function Hero() {
   return (
-    <section id="hero" className="flex h-screen items-center justify-center">
-      <h1>KnightHacks 2024</h1>
+    <section
+      id="hero"
+      className="relative flex h-screen flex-col items-center justify-center pt-16"
+    >
+      <h1 className="mb-2 text-4xl font-bold md:text-5xl lg:text-6xl">
+        KnightHacks 2024
+      </h1>
+      <div className="mb-4 flex gap-2">
+        <div>Engineering 1</div> • <div>Oct 4-6</div>
+      </div>
+      <div className="flex gap-2">
+        <Link href="/application/profile" passHref legacyBehavior>
+          <Button>Apply</Button>
+        </Link>
+        <Button variant="secondary" asChild>
+          <a href="mailto:sponsorship@knighthacks.org">Sponsor Us</a>
+        </Button>
+      </div>
+      <div className="relative w-full">
+        <Island />
+      </div>
     </section>
   );
 }
