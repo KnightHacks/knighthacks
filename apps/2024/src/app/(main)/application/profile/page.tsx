cf1f6cb715da9d22e4c89635c6eafcd8a32b9c78
Caleb Rivera <calebrossr@gmail.com>
2024-04-24 06:51:31 -0400

    Edge

--- apps/2024/src/app/(main)/application/profile/page.tsx
+++ apps/2024/src/app/(main)/application/profile/page.tsx
@@ -4,6 +4,8 @@ import { currentUser } from "@clerk/nextjs/server";
 import { trpc } from "~/trpc/server";
 import { ProfileForm } from "./profile-form";
 
+export const runtime = "edge";
+
 export default async function Profile() {
   const profile = await trpc.user.profile.query();
   if (profile) redirect("/application/survey");
--- apps/2024/src/app/(main)/application/survey/page.tsx
+++ apps/2024/src/app/(main)/application/survey/page.tsx
@@ -3,6 +3,8 @@ import { redirect } from "next/navigation";
 import { trpc } from "~/trpc/server";
 import { SurveyForm } from "./survey-form";
 
+export const runtime = "edge";
+
 export default async function Survey() {
   const application = await trpc.hacker.application.query();
   if (application) redirect("/dashboard");
