fd44da5bd5b09682663e753bbd02ab77b631e382
Caleb Rivera <calebrossr@gmail.com>
2024-05-01 00:49:11 -0400

    Format

--- apps/admin/src/app/not-found.tsx
+++ apps/admin/src/app/not-found.tsx
@@ -8,7 +8,9 @@ export default async function NotFound() {
   return (
     <div className="mx-auto flex min-h-screen flex-col justify-center">
       <h1 className="mb-2 text-3xl font-bold">Oops!</h1>
-      <p className="mb-4">Looks like you&apos;ve stumbled across a non-existent page.</p>
+      <p className="mb-4">
+        Looks like you&apos;ve stumbled across a non-existent page.
+      </p>
       <Link href="/" passHref legacyBehavior>
         <Button>Go back home</Button>
       </Link>
