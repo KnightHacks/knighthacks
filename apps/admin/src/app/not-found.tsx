321eabcfc35d2048f2fb000a872270742ab12d66
Caleb Rivera <calebrossr@gmail.com>
2024-05-01 00:46:35 -0400

    Configure not found pages

--- apps/admin/src/app/not-found.tsx
+++ apps/admin/src/app/not-found.tsx
@@ -8,9 +8,9 @@ export default async function NotFound() {
   return (
     <div className="mx-auto flex min-h-screen flex-col justify-center">
       <h1 className="mb-2 text-3xl font-bold">Oops!</h1>
-      <p className="mb-4">Looks like you&apos;ve gone adrift!</p>
+      <p className="mb-4">Looks like you&apos;ve stumbled across a non-existent page.</p>
       <Link href="/" passHref legacyBehavior>
-        <Button>Sail back home</Button>
+        <Button>Go back home</Button>
       </Link>
     </div>
   );
