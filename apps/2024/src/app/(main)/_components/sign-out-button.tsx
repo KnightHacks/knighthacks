760f849e4a7b3850fe80b48c77a2909916bc7f35
Caleb Rivera <calebrossr@gmail.com>
2024-05-03 06:32:53 -0400

    Redirect to home on sign out

--- apps/2024/src/app/(main)/_components/sign-out-button.tsx
+++ apps/2024/src/app/(main)/_components/sign-out-button.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { useAuth } from "@clerk/nextjs";
 
 import { Button } from "@knighthacks/ui/button";
 
 export function SignOutButton() {
   const { signOut } = useAuth();
+  const router = useRouter();
 
-  return <Button onClick={() => signOut()}>Sign Out</Button>;
+  return (
+    <Button onClick={() => signOut(() => router.push("/"))}>Sign Out</Button>
+  );
 }
