423bbd978cd6809e866da4c5d4d58e103385dd86
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-06-01 16:10:25 -0400

    Set about page and text and adjust sponsor page for mobile

--- apps/2024/src/app/(main)/_components/sections/about.tsx
+++ apps/2024/src/app/(main)/_components/sections/about.tsx
@@ -1,7 +1,27 @@
 export function About() {
   return (
     <section id="about" className="flex h-screen items-center justify-center">
-      <h1>About</h1>
+      <section
+        id="map"
+        className="flex h-auto flex-col items-center justify-center"
+        style={{
+          backgroundImage: "url(map.svg)",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          height: 600,
+          width: 1000,
+        }}
+      >
+        <h1 className="font-k2d text-center text-[40px] font-bold text-[#8E3B46]">
+          About <br /> KnightHacks
+        </h1>
+        <p className="font-k2d mb-20 mt-10 text-center text-[35px] font-semibold leading-[65px] text-[#8E3B46]">
+          Lorem ipsum dolor sit amet, consectetur <br /> adipiscing elit, sed do
+          eiusmod tempor
+          <br /> incididunt ut labore et dolore magna aliqua. Ut <br /> enim ad
+          minim veniam, quis nostrud <br /> exercitation.
+        </p>
+      </section>
     </section>
   );
 }
