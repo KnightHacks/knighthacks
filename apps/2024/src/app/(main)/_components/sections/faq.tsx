40aa9537c65dfef829ceaca362a6ffc1088d3037
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-06-23 10:39:45 -0400

    header

--- apps/2024/src/app/(main)/_components/sections/faq.tsx
+++ apps/2024/src/app/(main)/_components/sections/faq.tsx
@@ -1,7 +1,9 @@
 export function FAQ() {
   return (
     <section id="faq" className="flex h-screen items-center justify-center">
-      <h1>FAQ</h1>
+      <h1 className="font-k2d text-5xl font-semibold text-white">
+        Frequently Asked Questions
+      </h1>
     </section>
   );
 }
