36a524b44d5f5c75d0fa85b5ff583df90b541cfb
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-06-23 13:50:47 -0400

    questions and answers

--- apps/2024/src/app/(main)/_components/sections/faq.tsx
+++ apps/2024/src/app/(main)/_components/sections/faq.tsx
@@ -1,9 +1,73 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+
 export function FAQ() {
+  const FAQs: Record<string, string> = {
+    "What is a Hackathon?":
+      "A hackathon is a weekend-long event where students come together to learn the latest technologies and build innovative projects. These projects can range from involving web or mobile development to building a hardware project, or anything in between. Hackathons are a great way to learn new skills, meet new people, and have fun! Additionally, throughout the weekend we will be hosting workshops, fun social events, networking opportunities with sponsors, free food & swag, and so much more.",
+    "How long is Knight Hacks?":
+      "Knighthacks is a 36-hour hackathon, beginning at 5pm on Friday (check-in runs from 5-8pm, with the opening ceremony starting at 8pm) and ending at 6pm on Sunday. We encourage you to work on your project for as long as you can during this time.",
+    "Who can attend Knight Hacks?":
+      "If you’re currently a college student or have graduated in the past year, you're more than welcome to attend!",
+    "Do I need to have a team?":
+      "Not at all! You can be a lone wolf, come with a team (no more than four people), or join some teams at KnightHacks. We’ll also have team building activities to help you find the right teammates!",
+    "How much experience do I need?":
+      "None! We welcome students from all academic backgrounds and skill levels, so don’t be afraid to come and join us! We’ll have introductory workshops for you to learn new skills, industry mentors to help you out, and great tools to build your projects. Whether you’ve never coded before or have lots of experience, there’s a place for you at KnightHacks!",
+  };
+
+  const initialDropStates: Record<string, boolean> = Object.keys(FAQs).reduce(
+    (acc: Record<string, boolean>, key: string) => {
+      acc[key] = false;
+      return acc;
+    },
+    {},
+  );
+
+  const [dropStates, setDropStates] =
+    useState<Record<string, boolean>>(initialDropStates);
+
+  const handleToggle = (key: string) => {
+    setDropStates((prevState) => ({
+      ...prevState,
+      [key]: !prevState[key],
+    }));
+  };
+
   return (
-    <section id="faq" className="flex h-screen items-center justify-center">
-      <h1 className="font-k2d text-5xl font-semibold text-white">
+    <section
+      id="faq"
+      className="flex h-screen flex-col items-center justify-center"
+    >
+      <h1 className="font-k2d text-center text-5xl font-semibold text-black">
         Frequently Asked Questions
       </h1>
+      <div className="p-5">
+        {Object.entries(FAQs).map(([key, value]) => (
+          <div
+            key={key}
+            className="flex w-[1000px] flex-col items-start rounded-[20px] bg-[#7CCCFF] p-5"
+          >
+            <div className="flex flex-row items-center">
+              <button
+                onClick={() => handleToggle(key)}
+                className="focus:outline-none"
+              >
+                <Image
+                  width={50}
+                  height={50}
+                  src="/arrow.svg"
+                  alt="arrow"
+                  className={`transition-transform duration-100 ${dropStates[key] ? "rotate-90" : ""}`}
+                />
+              </button>
+              <p className="font-k2d text-2xl font-semibold">{key}</p>
+            </div>
+            <p>{dropStates[key] ? value : ""}</p>
+          </div>
+        ))}
+      </div>
     </section>
   );
 }
