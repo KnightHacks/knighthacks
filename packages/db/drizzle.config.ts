2af64c4e085ff9d2b798f2d9ff2cd21ef942d40b
Caleb Rivera <calebrossr@gmail.com>
2024-06-24 00:47:58 -0700

    Fix schema path

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   dialect: "sqlite",
-  out: "./migrations",
-  schema: ["./src/schemas/hackathon.ts", "./src/schemas/socials.ts"],
+  schema: "./src/schema.ts",
   driver: "turso",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
