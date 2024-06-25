c9e4e2850e2a101a996d3410bfebc1c78a8a1ba5
Caleb Rivera <calebrivera@meta.com>
2024-06-25 01:28:00 -0700

    Remove async on NavbarWrapper

--- apps/2024/src/app/(main)/_components/navbar-wrapper.tsx
+++ apps/2024/src/app/(main)/_components/navbar-wrapper.tsx
@@ -2,7 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 
 import { Navbar } from "./navbar";
 
-export async function NavbarWrapper() {
+export function NavbarWrapper() {
   const { userId } = auth();
   return <Navbar userId={userId} />;
 }
