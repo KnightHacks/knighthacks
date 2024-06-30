e3dc461104ced36c46d551cc5f52b02f0b0eb7eb
Caleb Rivera <calebrossr@gmail.com>
2024-06-29 18:37:15 -0700

    Scaffold dashboard with tabs

--- apps/2024/src/app/(main)/dashboard/page.tsx
+++ apps/2024/src/app/(main)/dashboard/page.tsx
@@ -1,5 +1,18 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@knighthacks/ui/tabs";
+
 export const runtime = "edge";
 
 export default function Dashboard() {
-  return <div className="px-8 pt-20">Dashboard</div>;
+  return (
+    <div className="px-8 pt-20">
+      <Tabs defaultValue="profile">
+        <TabsList>
+          <TabsTrigger value="profile">Profile</TabsTrigger>
+          <TabsTrigger value="application">Application</TabsTrigger>
+        </TabsList>
+        <TabsContent value="profile">Profile content</TabsContent>
+        <TabsContent value="application">Application content</TabsContent>
+      </Tabs>
+    </div>
+  );
 }
