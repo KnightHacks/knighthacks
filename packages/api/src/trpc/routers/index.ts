d083e5a15e0a436c205c6e3f7e0469e00e4afba7
Caleb Rivera <calebrivera@meta.com>
2024-06-29 21:52:20 -0700

    Merge routers

--- packages/api/src/trpc/routers/index.ts
+++ packages/api/src/trpc/routers/index.ts
@@ -1,18 +1,15 @@
-import { createTRPCRouter } from "../init";
-import { publicProcedure } from "../procedures";
+import { mergeRouters } from "@trpc/server/unstable-core-do-not-import";
+
 import { hackathonRouter } from "./hackathons";
 import { hackerRouter } from "./hackers";
 import { sponsorRouter } from "./sponsors";
 import { userRouter } from "./users";
 
-export const router = createTRPCRouter({
-  hello: publicProcedure.query(() => {
-    return "Hello from Hono!";
-  }),
-  user: userRouter,
-  hackathon: hackathonRouter,
-  hacker: hackerRouter,
-  sponsor: sponsorRouter,
-});
+export const router = mergeRouters(
+  userRouter,
+  hackathonRouter,
+  hackerRouter,
+  sponsorRouter,
+);
 
 export type AppRouter = typeof router;
