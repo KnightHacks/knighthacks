a51084579f1b9874ca9d527f4ab93f3391f03d0f
Dylan Vidal <dylanvidal1204@gmail.com>
2024-06-29 20:38:29 -0400

    New navbar + MLH Badge changes

--- apps/2024/src/app/(main)/_components/navbar.tsx
+++ apps/2024/src/app/(main)/_components/navbar.tsx
@@ -25,63 +25,87 @@ export function Navbar({ userId }: { userId: string | null }) {
   });
 
   return (
-    <motion.nav
-      animate={{ y: hidden ? "-100%" : 0 }}
-      transition={{ type: "tween", duration: 0.2 }}
-      className={cn(
-        "fixed z-50 flex h-16 w-full items-center justify-between bg-background bg-[url('/sky.svg')] px-2 md:px-4",
-      )}
-    >
-      <div className="flex items-center md:absolute md:left-4">
-        <Link href="/#hero" passHref legacyBehavior>
-          <Button
-            variant="ghost"
-            className="relative flex items-center text-xl font-bold"
-          >
-            <Image src="/kh-logo.svg" alt="kh-logo" width={200} height={300} />
-          </Button>
-        </Link>
-      </div>
-      <ul className="hidden flex-1 items-center justify-center space-x-4 md:flex">
-        <li>
-          <Link href="/#about" passHref legacyBehavior>
-            <Button variant="ghost" className="text-white">
-              About
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="/#faq" passHref legacyBehavior>
-            <Button variant="ghost" className="text-white">
-              FAQ
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="/#sponsors" passHref legacyBehavior>
-            <Button variant="ghost" className="text-white">
-              Sponsors
-            </Button>
-          </Link>
-        </li>
-        <li>
-          <Link href="/#contact" passHref legacyBehavior>
-            <Button variant="ghost" className="text-white">
-              Contact
-            </Button>
-          </Link>
-        </li>
-      </ul>
-      <div className="hidden items-center md:absolute md:right-4 md:flex">
-        {userId ? (
-          <SignOutButton />
-        ) : (
-          <Link href="/sign-in" passHref legacyBehavior>
-            <Button>Sign In</Button>
-          </Link>
+    <>
+      <motion.nav
+        animate={{ y: hidden ? "-100%" : 0 }}
+        transition={{ type: "tween", duration: 0.2 }}
+        className={cn(
+          "fixed z-50 flex h-16 w-full items-center bg-slate-950 px-2 md:px-4",
         )}
-      </div>
-      <MobileSheet userId={userId} />
-    </motion.nav>
+      >
+        <MobileSheet userId={userId} />
+        <div className="flex items-center">
+          <Link href="/#hero" passHref legacyBehavior>
+            <Button
+              variant="ghost"
+              className="relative flex items-center text-xl font-bold"
+            >
+              <Image
+                src="/kh-logo.svg"
+                alt="kh-logo"
+                width={200}
+                height={300}
+              />
+            </Button>
+          </Link>
+        </div>
+        <div className="hidden flex-1 justify-center md:flex">
+          <ul className="flex items-center space-x-4">
+            <li>
+              <Link href="/#about" passHref legacyBehavior>
+                <Button variant="ghost" className="text-white">
+                  About
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/#faq" passHref legacyBehavior>
+                <Button variant="ghost" className="text-white">
+                  FAQ
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/#sponsors" passHref legacyBehavior>
+                <Button variant="ghost" className="text-white">
+                  Sponsors
+                </Button>
+              </Link>
+            </li>
+            <li>
+              <Link href="/#contact" passHref legacyBehavior>
+                <Button variant="ghost" className="text-white">
+                  Contact
+                </Button>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center space-x-4">
+          <div className="hidden md:flex">
+            {userId ? (
+              <SignOutButton />
+            ) : (
+              <Link href="/sign-in" passHref legacyBehavior>
+                <Button>Sign In</Button>
+              </Link>
+            )}
+          </div>
+          <motion.div
+            animate={{ y: hidden ? "-250%" : 0 }}
+            transition={{ type: "tween", duration: 0.35 }}
+            className="flex items-center"
+          >
+            <Image
+              src="/mlh-badge.svg"
+              alt="MLH Badge"
+              width={100}
+              height={200}
+              className="-mb-24 object-contain md:-mb-28"
+            />
+          </motion.div>
+        </div>
+      </motion.nav>
+    </>
   );
 }
