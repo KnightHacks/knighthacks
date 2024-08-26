46324ea823216132deac37851d2c5110a08ff2fb
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-08-26 11:25:29 -0400

    added status conditional

--- apps/2024/src/app/(main)/dashboard/page.tsx
+++ apps/2024/src/app/(main)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@knighthacks/ui/tabs";
 
 import { trpc } from "~/trpc/server";
+import { ConfirmStatusForm } from "./_components/confirm-status-form";
 import { UpdateProfileForm } from "./_components/update-profile-form";
 import { UpdateSurveyForm } from "./_components/update-survey-form";
 
@@ -19,14 +20,24 @@ export default async function Dashboard() {
       <Tabs defaultValue="profile">
         <TabsList>
           <TabsTrigger value="profile">Profile</TabsTrigger>
-          <TabsTrigger value="application">Application</TabsTrigger>
+          {user.hackers[0]?.status !== "accepted" ? (
+            <TabsTrigger value="application">Application</TabsTrigger>
+          ) : (
+            <TabsTrigger value="confirmation">Confirmation</TabsTrigger>
+          )}
         </TabsList>
         <TabsContent value="profile">
           <UpdateProfileForm user={user} />
         </TabsContent>
-        <TabsContent value="application">
-          <UpdateSurveyForm application={application} />
-        </TabsContent>
+        {user.hackers[0]?.status !== "accepted" ? (
+          <TabsContent value="application">
+            <UpdateSurveyForm application={application} />
+          </TabsContent>
+        ) : (
+          <TabsContent value="confirmation">
+            <ConfirmStatusForm />
+          </TabsContent>
+        )}
       </Tabs>
     </div>
   );
