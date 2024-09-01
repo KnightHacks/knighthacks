8e446ad67106825342fe11a501fcd00cfa563b16
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-09-01 12:30:12 -0400

    trpc route setup (doesn't work)

--- packages/api/src/trpc/routers/email.ts
+++ packages/api/src/trpc/routers/email.ts
@@ -1,26 +1,29 @@
-import { z } from "zod";
-import { publicProcedure } from "../procedures";
 import type { TRPCRouterRecord } from "@trpc/server";
+import { z } from "zod";
+
+import { publicProcedure } from "../procedures";
+
 // import { renderEmailAcceptance} from "@knighthacks/utils";
 
 const user = z.object({
-    id: z.number(),
-    email: z.string().min(1, { message: "Required" }).email("Invalid email"),
+  id: z.number(),
+  userId: z.number(),
+  email: z.string().min(1, { message: "Required" }).email("Invalid email"),
   firstName: z.string().min(1, { message: "Required" }),
   lastName: z.string().min(1, { message: "Rquired" }),
   clerkID: z.string().min(1, { message: "Required" }),
-})
+});
 
 export const emailRouter = {
   sendAcceptanceEmail: publicProcedure
-  .input(user)
-  .mutation(async ({ ctx, input }) => {
-    console.log("input: ", input.email)
-    await ctx.resend.emails.send({
+    .input(user)
+    .mutation(async ({ ctx, input }) => {
+      console.log("input: ", input.email);
+      await ctx.resend.emails.send({
         from: "status@knighthacks.org",
         to: input.email,
         subject: "Knighthacks Acceptance",
         html: "lol",
-      })
-  }),
+      });
+    }),
 } satisfies TRPCRouterRecord;
