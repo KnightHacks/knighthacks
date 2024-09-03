0a1866e7af3648d9608ae0c53b9acdb74350f6f4
dany <71732131+DanyPalma@users.noreply.github.com>
2024-09-03 18:20:43 -0400

    Update env.ts

--- apps/admin/src/env.ts
+++ apps/admin/src/env.ts
@@ -12,7 +12,10 @@ export const env = createEnv({
    * Specify your server-side environment variables schema here.
    * This way you can ensure the app isn't built with invalid env vars.
    */
-  server: {},
+  server: {
+    RESEND_API_KEY: z.string(),
+  },
+
   /**
    * Specify your client-side environment variables schema here.
    * For them to be exposed to the client, prefix them with `NEXT_PUBLIC_`.
