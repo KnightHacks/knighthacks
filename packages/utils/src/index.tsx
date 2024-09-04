9f4e171dd4cf9a731223ca8c5f10bb9f7fa39f1b
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-09-03 21:59:48 -0700

    mark email template

--- packages/utils/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@react-email/components";
-
-import EmailAcceptance from "./emails/email-acceptance";
-
-export const renderEmailAcceptance = (firstname: string) => {
-  return render(<EmailAcceptance firstName={firstname} />);
-};
