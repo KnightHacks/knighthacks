7e001c55b95e967e0709129c54f9655f656e6d36
dany <71732131+DanyPalma@users.noreply.github.com>
2024-09-07 18:14:05 -0400

    Delete render-email-acceptance.tsx

--- apps/admin/emails/render-email-acceptance.tsx
+++
@@ -1,7 +0,0 @@
-import { render } from "@react-email/render";
-
-import EmailAcceptance from "./email-acceptance";
-
-export const renderEmailAcceptance = (firstname: string) => {
-  return render(<EmailAcceptance firstName={firstname} />);
-};
