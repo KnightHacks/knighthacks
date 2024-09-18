c6236fab6d45231cff258fd4af0def04ef6d65d1
Lewin Bobda <116130662+Lewin-B@users.noreply.github.com>
2024-09-17 21:15:31 -0700

    Add waitlist email

--- apps/admin/emails/render-email-acceptance.tsx
+++ apps/admin/emails/render-email-acceptance.tsx
@@ -1,7 +1,12 @@
 import { render } from "@react-email/components";
 
 import EmailAcceptance from "./email-acceptance";
+import EmailWaitlist from "./email-waitlist";
 
 export const renderEmailAcceptance = (firstname: string) => {
   return render(<EmailAcceptance firstName={firstname} />);
+};
+
+export const renderEmailWaitlist = (firstname: string) => {
+  return render(<EmailWaitlist firstName={firstname} />);
 };
---
+++ apps/admin/src/app/api/send-waitlist-email/route.ts
@@ -0,0 +1,39 @@
+import { z } from "@knighthacks/validators";
+import { renderEmailWaitlist } from "emails/render-email-acceptance";
+import { Resend } from "resend";
+
+import { env } from "~/env";
+
+const resend = new Resend(env.RESEND_API_KEY);
+
+export const runtime = "edge";
+
+const user = z.object({
+  id: z.number(),
+  email: z.string().min(1, { message: "Required" }).email("Invalid email"),
+  firstName: z.string().min(1, { message: "Required" }),
+  lastName: z.string().min(1, { message: "Rquired" }),
+  clerkID: z.string().min(1, { message: "Required" }),
+});
+
+export async function POST(request: Request) {
+  const body = (await request.json()) as Response;
+  const input = user.parse(body);
+
+  try {
+    const { data, error } = await resend.emails.send({
+      from: "no-reply-status@knighthacks.org",
+      to: input.email,
+      subject: "Knighthacks Waitlist",
+      html: renderEmailWaitlist(input.firstName),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
